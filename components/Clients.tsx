16d55e8b1111ae58867db4e2eceaaf7387d01083
Manne Rithvk <mannerithvik708@gmail.com>
2024-07-18 21:34:30 +0530

    first commit

---
+++ components/Clients.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+import { companies, testimonials } from "@/data";
+import { InfiniteMovingCards } from "./ui/InfiniteCards";
+
+const Clients = () => {
+  return (
+    <section>
+    </section>
+  );
+};
+
+export default Clients;
---
+++ data/index.ts
@@ -0,0 +1,214 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently working on an internship portal for students of my college",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Clone of Delta cubes",
+    des: "Made a clone of a Delta cubes company's website using NextJS, Tailwind, and TypeScript.",
+    img: "/img1.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg",],
+    
+  },
+  {
+    id: 2,
+    title: "FCC prototype website",
+    des: "Bulit a prototype website for the Finance and consulting club using NextJS, Tailwind, and TypeScript.",
+    img: "/img2.jpeg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg",],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Career Quiz app",
+    des: "Built a web app which asks you basic questions about some techinical concepts and gives you an idea of which career path you should pursue.",
+    img: "/img3.jpeg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg",],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "More Projects on the way...",
+    des: "",
+    img: "https://t3.ftcdn.net/jpg/03/79/03/48/360_F_379034833_c5P5Q5ZB1fF711lUEmBOkFlFPTVQYA11.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Web dev intern - Exposys Data labs",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web dev coordinator at Ecell-IIT hyderabad",
+    desc: "Working as a web dev coordinator at Ecell-IIT Hyderabad.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Web dev coordinator at FCC-IIT hyderabad",
+    desc: "Working as a web dev coordinator at FCC-IIT Hyderabad.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Web dev coordinator at Ir-cell IIT Hyderabad",
+    desc: "Working as a web dev coordinator at Ir-cell IIT Hyderabad.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href: "https://github.com/rithvik-2006",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    href: "https://www.linkedin.com/in/manne-rithvik-708iith/",
+  },
+];
